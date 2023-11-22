3892fc391a6227775705cc6a8d01d2588a0a76e5
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-11-22 14:03:42 -0300

    Correção da criação de lista e criando exibição
    Correção de erros na criação da Lista Invertida.
    Criação do Método de impressão da Lista invertida ordenadamente.

--- src/listaInvertida.java
+++ src/listaInvertida.java
@@ -1,7 +1,10 @@
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class listaInvertida {
 
@@ -11,16 +14,17 @@ public class listaInvertida {
 
     Map<String, Integer> contagemNomes = new HashMap<>(); //Mapa servirá para contabilizar a incidencia dos nomes, podendo ser atualizada sempre que necessario
 
+    
     void criarListaNome(){
         try {
             
             arquivoOrigem = new RandomAccessFile(pathOrigem, "rw"); //Abrindo arquivo para leitura
             int quantidadeIDs = 0;
-            int IDloop = 0;
+            int IDloop = 1;
 
             quantidadeIDs = arquivoOrigem.readInt(); //Lendo a quantidade de IDs, provisionado pelo metadados
 
-            while (IDloop >= quantidadeIDs) {
+            while (IDloop < quantidadeIDs) {
                 if(arquivoOrigem.readChar() != '*'){ //Checando lápide
                     /*
                     * Primeiro, será feito a leitura do arquivo
@@ -45,22 +49,45 @@ public class listaInvertida {
                 }   
                 else{
                     arquivoOrigem.skipBytes(arquivoOrigem.readInt()); //Se estiver deletado, pula X bytes para mudar de registro, como indicado no inicio do registro.
+                    IDloop++; //Pula o ID atual
                 } 
             }
         } 
+        catch (EOFException e){} //Resolução para o erro EOF ao terminar de ler o arquivo todo
+        
         catch (IOException e) {
             System.out.println("Houve um erro ao tentar abrir o arquivo driversDB:");
             e.printStackTrace();
         } 
-
+        System.out.println("Lista criada com sucesso!\n");
     }
 
     void pesquisarListaNome(){
-
+        
     }
 
+    
     void imprimirListaInvertida(){
-        
+        //Verificando se o Map foi carregado com
+        if (contagemNomes.size() == 0) {
+            System.out.println("Crie uma lista invertida antes de imprimir!");
+            
+        }
+        else{
+            Map<String, Integer> mapaOrdenado = contagemNomes.entrySet().stream() // Criando uma Stream das entradas do mapa
+                .sorted(Map.Entry.comparingByValue()) // Ordena as entradas do Stream comparando ao Map.Entry
+                .collect(Collectors.toMap( // Coleta os resultados da ordenação em um OUTRO Map
+                    Map.Entry::getKey, // Obtendo a String
+                    Map.Entry::getValue, // Obtendo o int
+                    (oldValue, newValue) -> oldValue, // Resolução de qualquer conflito caso apareçam chaves iguais
+                    LinkedHashMap::new)); // Criação do novo Map para o recebimento da Stream ordenada
+
+            //Impressão das String ordenadas
+            System.out.println("Incidencia dos nomes:");
+            for(Map.Entry<String, Integer> entry : mapaOrdenado.entrySet()){
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
     }
 
 }
