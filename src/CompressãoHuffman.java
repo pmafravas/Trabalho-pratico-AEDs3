7e6eb746c5d04aa8cad750eef05db030fe722ed8
Pedro Mafra <pedro.m.vas@hotmail.com>
2023-11-25 16:05:57 -0300

    Desenvolvimento da compressão de Huffman

--- src/CompressãoHuffman.java
+++ src/CompressãoHuffman.java
@@ -12,6 +12,29 @@ public class CompressãoHuffman {
     
     void contagemDeSimbolos() throws IOException {
         arquivo = new RandomAccessFile(dbPath, "rw"); //Abrindo arquivo para realizar contabilização
-        
+        arquivo.seek(0); //Apontado para o inicio do arquivo
+        Integer metadados = arquivo.readInt(); //Lendo todo o metadado
+
+        /*
+         * Leitura do metadados
+         * Lendo um int e o convertendo para um char com auxilio de uma String
+         */
+        String simbolo = metadados.toString();
+        char[] c = new char[simbolo.length()];
+        for(int i = 0; i < simbolo.length(); i++){
+            c[i] = simbolo.charAt(i); //Pegando o símbolo da String e jogando para um char
+            inserirNoHash(c[i]); //Inserindo simbolo no Hash
+        }
+    }
+
+    void inserirNoHash(Character c) {
+        if (!frequencia.containsKey(c)) { //Checando se o HashMap já contem o símbolo
+            frequencia.put(c, 1); //Como o Hash não contem o símbolo, ele será adicionado com a frequencia de 1
+        }
+        else{
+            Integer valorInt = frequencia.get(c); //Caso o simbolo existe, ele será pego do Hash
+            valorInt++; //Aumentando sua frenquencia
+            frequencia.put(c, valorInt); //E devolvido com o Int incrementado
+        }
     }
 }
